dbe32e8f2553f97a9c89b05723bc1111891e85ce
lxiaocode <lxiaocode@qq.com>
2023-05-31 23:47:45 +0800

    init

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+int main() 
+{
+    char buff[255];
+    int error;
+
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    while (fgets(buff, sizeof(buff), stdin) != NULL)
+    {
+        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
+        if (error)
+        {
+            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);
+        }
+    }
+
+    lua_close(L);
+    return 0;
+}
